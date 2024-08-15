df5f5286d5ad6c1040675f170cafb987694e821d
PanchoSaa <hugosaa@yahoo.com.ar>
2024-08-15 13:34:21 -0300

    cambio input

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ parrafo.innerHTML=`Indica un número del 1 al 10`;*/
 
 let numeroSecreto=generarNumeroSecreto();
 
+
 function asignarTextoElemento(elemento,texto){
     let elementoHTML=document.querySelector(elemento);
     elementoHTML.innerHTML=texto;
@@ -14,9 +15,12 @@ function asignarTextoElemento(elemento,texto){
 asignarTextoElemento(`h1`,`juego del número secreto`);
 asignarTextoElemento(`p`,`indica un número del 1 al 10`);
 
-function intentoDeUsuario(){
-  alert('click desde la función');  
+function verificarIntento(){
+  let numeroDeUsuario=document.getElementById(valorUsuario).nodeValue;
+  console.log(numeroSecreto);
+  console.log(numeroDeUsuario);  
+  console.log(numeroSecreto==numeroDeUsuario);
 }
-function generarNumerosecreto() {
-    return Math.floor(Math.random*10)+1;
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
