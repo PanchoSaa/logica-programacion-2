0b58f3e088ee042b339a63a93189e6e80457b9ee
PanchoSaa <hugosaa@yahoo.com.ar>
2024-08-15 12:56:04 -0300

    funciones agregadas

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-let titulo=document.querySelector(`h1`);
+/*let titulo=document.querySelector(`h1`);
 titulo.textContent=`juego del número secreto`;
 let parrafo=document.querySelector(`p`);
-parrafo.innerHTML=`Indica un número del 1 al 10`;
+parrafo.innerHTML=`Indica un número del 1 al 10`;*/
+
+let numeroSecreto=generarNumeroSecreto();
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+
+};
+
+asignarTextoElemento(`h1`,`juego del número secreto`);
+asignarTextoElemento(`p`,`indica un número del 1 al 10`);
+
 function intentoDeUsuario(){
   alert('click desde la función');  
+}
+function generarNumerosecreto() {
+    return Math.floor(Math.random*10)+1;
 }
